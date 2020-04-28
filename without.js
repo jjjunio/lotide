215b2de4c911d6718c4b6fe1e2c147f1523180ae
Jason Junio <jjjunio@gmail.com>
2020-04-28 23:57:45 +0000

    solution to the imoplement without lotide project

---
+++ without.js
@@ -0,0 +1,34 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const without = function(sourceArr, removeArr) {
+  let finalAnswer = [];
+  for (i = 0; i < sourceArr.length; i++) {
+    if (sourceArr[i] !== removeArr[i]) {
+      finalAnswer.push(sourceArr[i]);
+    }
+  }
+  return finalAnswer;
+};
+
+console.log(without([1, 2, 3], [1]));// => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
