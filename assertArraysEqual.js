af87643d13b9768e08d40c5f835da2e732419b42
Jason Junio <jjjunio@gmail.com>
2020-04-28 23:10:04 +0000

    Implement assertArraysEqual solution for lotide project

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    return (`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    return (`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
