ba1f2742b3fd0da52b0c6af3d7cbd42fcf22b4c4
Jason Junio <jjjunio@gmail.com>
2020-04-29 23:10:32 +0000

    Solution to Implement Letter Positions Lotide project

---
+++ letterPositions.js
@@ -0,0 +1,34 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  // sentence = sentence.split(' ').join('');
+  
+  for (let i = 0; i < sentence.length; i++) {
+    if (!results[sentence[i]]) {
+      results[sentence[i]] = [];
+    }
+    results[sentence[i]].push(i);
+  }
+  delete results[" "];
+  return results;
+};
+
+
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+console.log(letterPositions('lighthouse in the house'));
+// assertArraysEqual(letterPositions('hello').e, [1]);
