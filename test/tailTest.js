cceba40489215bdf02313c1a9c3fdbf087164df3
Jason Junio <jjjunio@gmail.com>
2020-05-04 19:50:55 +0000

    refactored tailTest for unit testing in mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,25 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-const words2 = [];
-tail(words2); // no need to capture the return value since we are not checking it
-assertEqual(words2.length, 0); // original array should still have 3 elements!
+describe("this tests for the tail or number of elements in the tail of an array", () => {
 
-const words3 = ["Yo Yo"];
-tail(words2); // no need to capture the return value since we are not checking it
-assertEqual(words3.length, 1); // original array should still have 3 elements!
+  it("returns 3 for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+
+  it("returns 0 for []", () => {
+    const words2 = [];
+    tail(words2);
+    assert.strictEqual(words2.length, 0);
+  });
+
+  it("returns 1 for ['Yo Yo']", () => {
+    const words3 = ["Yo Yo"];
+    tail(words3);
+    assert.strictEqual(words3.length, 1);
+  });
+
+});//end
