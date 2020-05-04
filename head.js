2e68f07da10685f9ac6c8582f16b68b7e80c92a2
Jason Junio <jjjunio@gmail.com>
2020-05-04 13:19:47 +0000

    refactored head and assertEqual functions

--- head.js
+++ head.js
@@ -1,12 +1,7 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([5]), 5);
+module.exports = head;
